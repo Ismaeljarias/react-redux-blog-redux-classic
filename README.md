7f45ad1ebada36b1e5851c8ad695db291ce3b062
Ismael Arias <ismaeljarias@gmail.com>
2020-08-05 09:29:11 -0400

    Remove Lodash

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 This is a React classic app, using Class components to use the old style way to implement Redux and Redux Thunk without any hooks
 It is just a fetch from an api and bring some post and users and a simple implementation of Lodash to help us with some Memoization.
+
+New update: I decide to remove Lodash since this memoization issue it can be resolve with simple Vanilla JS.
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,19 @@
-import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPost());
 
+  const uniqueUsers = [...new Set(getState().posts.map((post) => post.userId))];
+  uniqueUsers.forEach((id) => dispatch(fetchUser(id)));
+
   // const userIds = _.uniq(_.map(getState().posts, "userId"));
   // userIds.forEach((id) => dispatch(fetchUser(id)));
 
-  _.chain(getState().posts)
-    .map("userId")
-    .uniq()
-    .forEach((id) => dispatch(fetchUser(id)))
-    .value();
+  // _.chain(getState().posts)
+  //   .map("userId")
+  //   .uniq()
+  //   .forEach((id) => dispatch(fetchUser(id)))
+  //   .value();
 };
 
 export const fetchPost = () => {
